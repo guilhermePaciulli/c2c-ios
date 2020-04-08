437bc578ef41bd36f589c5f5dec137e635d1d062
DevilInside <gpaciulli100@gmail.com>
2020-04-08 20:54:24 -0300

    feat(create-product): view ctrl, sb and view model

---
+++ C2C/C2C/Products/Create/View/CreateProductTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CreateProductTableViewController.swift
+//  C2C
+//
+//  Created by Guilherme Paciulli on 08/04/20.
+//  Copyright © 2020 Guilherme Paciulli. All rights reserved.
+//
+
+import UIKit
+
+class CreateProductTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ C2C/C2C/Products/Create/ViewModel/CreateProductViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  CreateProductViewModel.swift
+//  C2C
+//
+//  Created by Guilherme Paciulli on 08/04/20.
+//  Copyright © 2020 Guilherme Paciulli. All rights reserved.
+//
+
+import Foundation
