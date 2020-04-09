b85bfe71922ae67052cde26e053d79f3e8ebf75a
DevilInside <gpaciulli100@gmail.com>
2020-04-08 21:19:15 -0300

    feat(account-viewmodel): create validations

--- C2C/C2C/Utils/ProductFields.swift
+++ C2C/C2C/Utils/ProductFields.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+
+enum ProductFields: CaseIterable {
+    case Name
+    case Description
+    case Price
+    
+    func validate(string: String) -> String? {
+        switch self {
+        case .Name:
+            return string.isEmpty ? "Name must not be empty" : nil
+        case .Description:
+            return string.isEmpty ? "Name must not be empty" : nil
+        case .Price:
+            if let price = Int(string) {
+                return price > 0 ? "Price must be greater than zero" : nil
+            }
+            return "Price is not valid"
+        }
+    }
+    
+}
