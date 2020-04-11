c313a7af8c32cfcb8912de0e29d456c58ce34918
DevilInside <gpaciulli100@gmail.com>
2020-04-11 15:21:20 -0300

    feature(address): create viewmodel and ctrler functions

--- C2C/C2C/Address/ViewModel/AddressViewModel.swift
+++ C2C/C2C/Address/ViewModel/AddressViewModel.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+
+protocol AddressViewModelProtocol {
+    func didTapToCancel()
+    func didTapToRegisterAddress()
+}
+
+class AddressViewModel: AddressViewModelProtocol {
+    
+    // MARK:- Properties
+    var interactor: UserInteractorProtocol
+    var coordinator: BasicCoordinationProtocol?
+    var view: AddressPresentable?
+    
+    // MARK:- Initialization
+    init(interactor: UserInteractorProtocol = UserInteractor()) {
+        self.interactor = interactor
+    }
+    
+    // MARK:- Delegate methods
+    func didTapToRegisterAddress() {
+        guard let view = self.view else { return }
+        let errors = AddressFields.allCases.compactMap({ return $0.validate(string: view.getField($0)) })
+        if errors.isEmpty /*, let acc = getAccount()*/ {
+        } else {
+            view.showAlert(withTitle: "Invalid fields", message: errors.joined(separator: ", "))
+        }
+    }
+    
+    
+    func didTapToCancel() {
+        coordinator?.presentPreviousStep()
+    }
+    
+    // MARK:- Private methods
+    private func getAccount() {
+    }
+    
+}
