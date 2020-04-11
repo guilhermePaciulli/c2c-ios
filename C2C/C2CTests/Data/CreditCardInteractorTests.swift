6fca38e44e55617f0291f87ed1ef3a85e5ab4814
DevilInside <gpaciulli100@gmail.com>
2020-04-11 18:17:07 -0300

    feature(credit-cards): requests and tests for that

---
+++ C2C/C2CTests/Data/CreditCardInteractorTests.swift
@@ -0,0 +1,95 @@
+//
+//  CreditCardInteractorTests.swift
+//  C2CTests
+//
+//  Created by Guilherme Paciulli on 11/04/20.
+//  Copyright © 2020 Guilherme Paciulli. All rights reserved.
+//
+
+@testable import C2C
+import Quick
+import Nimble
+
+class CreditCardInteractorTests: QuickSpec {
+    
+    var repository: CreditCardRepositoryMock = .init()
+    var subject: CreditCardInteractor = .init(repository: CreditCardRepositoryMock())
+    let mockedCreditCard: SaveCreditCard = .init(number: "4521009716146231", cvv: "385", owner: "Rogerinho do Ingá", expiration: "06/23")
+    
+    override func spec() {
+        
+        beforeEach {
+            self.repository = .init()
+            self.subject = .init(repository: self.repository)
+        }
+        
+        describe("CreditCardInteractor GET request") {
+            it("should return an address when successful") {
+                waitUntil { (done) in
+                    self.subject.get().done {
+                        expect($0).toNot(beNil())
+                        done()
+                    }.catch { XCTFail($0.localizedDescription); done() }
+                }
+            }
+            
+            it("should return handle request error") {
+                let message = "there was an error"
+                self.repository.responseError = .init(message: message)
+                
+                waitUntil { (done) in
+                    self.subject.get().done({ _ in XCTFail("the request should have failed"); done() })
+                        .catch({
+                            expect($0.localizedDescription).to(equal(message)); done()
+                        })
+                }
+            }
+        }
+        
+        describe("CreditCardInteractor POST request") {
+            it("should create an address when successful") {
+                waitUntil { (done) in
+                    self.subject.create(self.mockedCreditCard).done {
+                        expect($0).toNot(beNil())
+                        done()
+                    }.catch { XCTFail($0.localizedDescription); done() }
+                }
+            }
+            it("should return handle request error") {
+                let message = "there was an error"
+                self.repository.responseError = .init(message: message)
+                
+                waitUntil { (done) in
+                    self.subject.create(self.mockedCreditCard).done({ _ in XCTFail("the request should have failed"); done() })
+                        .catch({
+                            expect($0.localizedDescription).to(equal(message)); done()
+                        })
+                }
+            }
+        }
+        
+        describe("CreditCardInteractor PUT request") {
+            it("should create an address when successful") {
+                waitUntil { (done) in
+                    self.subject.update(self.mockedCreditCard).done {
+                        expect($0).toNot(beNil())
+                        done()
+                    }.catch { XCTFail($0.localizedDescription); done() }
+                }
+            }
+            it("should return handle request error") {
+                let message = "there was an error"
+                self.repository.responseError = .init(message: message)
+                
+                waitUntil { (done) in
+                    self.subject.update(self.mockedCreditCard).done({ _ in XCTFail("the request should have failed"); done() })
+                        .catch({
+                            expect($0.localizedDescription).to(equal(message)); done()
+                        })
+                }
+            }
+        }
+        
+    }
+    
+}
