2c74b09e720777933a05f52c1f3057eb6c562d1d
DevilInside <gpaciulli100@gmail.com>
2020-04-20 20:16:53 -0300

    trying to remove duplicated lines

--- C2C/C2CTests/Data/CreditCardInteractorTests.swift
+++ C2C/C2CTests/Data/CreditCardInteractorTests.swift
@@ -24,24 +24,29 @@ class CreditCardInteractorTests: QuickSpec {
         }
         
         describe("CreditCardInteractor GET request") {
-            it("should return an address when successful") {
+            it("should return an credit card when successful") {
                 waitUntil { (done) in
-                    self.subject.get().done {
-                        expect($0).toNot(beNil())
+                    self.subject.get().done { res in
+                        expect(res).toNot(beNil())
                         done()
-                    }.catch { XCTFail($0.localizedDescription); done() }
+                    }.catch { error in
+                        XCTFail(error.localizedDescription)
+                        done()
+                    }
                 }
             }
             
-            it("should return handle request error") {
-                let message = "there was an error"
+            it("should handle request error for this thing") {
+                let message = "there was an error fetching credit card"
                 self.repository.responseError = .init(message: message)
                 
                 waitUntil { (done) in
-                    self.subject.get().done({ _ in XCTFail("the request should have failed"); done() })
-                        .catch({
-                            expect($0.localizedDescription).to(equal(message)); done()
-                        })
+                    self.subject.get().done({ _ in
+                        XCTFail("the request did not fail :(")
+                        done()
+                    }).catch({ err in
+                        expect(err.localizedDescription).to(equal(message)); done()
+                    })
                 }
             }
         }
