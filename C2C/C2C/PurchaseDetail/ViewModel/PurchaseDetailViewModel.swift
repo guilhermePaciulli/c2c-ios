bbe5ffa32040b29b73792d0cbd4c2e65cbfce7b6
DevilInside <gpaciulli100@gmail.com>
2020-05-11 20:19:05 -0300

    classes and sb creations

---
+++ C2C/C2C/PurchaseDetail/ViewModel/PurchaseDetailViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  PurchaseDetailViewModel.swift
+//  C2C
+//
+//  Created by Guilherme Paciulli on 11/05/20.
+//  Copyright © 2020 Guilherme Paciulli. All rights reserved.
+//
+
+import Foundation
